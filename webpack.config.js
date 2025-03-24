70228d5c88d2ef342e794bc204c4573c01ca9ccc
pavlopavlopavlo <pavan.m.rami@gmail.com>
2025-03-23 23:00:28 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,57 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './client/src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpg|gif|svg|gltf|glb)$/,
-        use: [
-          {
-            loader: 'file-loader',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+  
+  return {
+    entry: './client/src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+      clean: true
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/'
+              presets: ['@babel/preset-env', '@babel/preset-react']
             }
           }
-        ]
-      },
-      {
-        test: /\.(vert|frag|glsl)$/,
-        use: ['raw-loader']
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(vert|frag|glsl)$/,
+          use: 'raw-loader'
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './client/public/index.html'
+      })
+    ],
+    devServer: {
+      static: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 3000,
+      hot: true,
+      proxy: {
+        '/api': 'http://localhost:5000',
+        '/socket.io': {
+          target: 'http://localhost:5000',
+          ws: true
+        }
       }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './client/public/index.html'
-    })
-  ],
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 3000,
-    proxy: {
-      '/api': 'http://localhost:5000',
-      '/socket.io': {
-        target: 'http://localhost:5000',
-        ws: true
-      }
-    }
-  }
+    },
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'eval-source-map'
+  };
 };
